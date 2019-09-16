1250c850ce263e053ded6b4c5c496fc3bffff856
Wagner Candido <42880595+wagnercandido@users.noreply.github.com>
2019-09-16 14:34:20 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://stf-pocka-backend.herokuapp.com',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://api-pocka.herokuapp.com'
+  // baseURL: 'http://localhost:3333',
 });
 
 export default api;
